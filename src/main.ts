fb1dd587ca384f8df8782e45cc80a78f6ecf342c
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-05-31 01:18:36 -0500

    style: change spacing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication
- } from '@nestjs/platform-express';
- 
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
