b569fb4ae0adec45ff64bac1903a18563035ebcc
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-05-31 00:45:16 -0500

    chore: config mongoose and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import database from './config/database';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [database],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: `mongodb://${config.get<string>('DB_USER')}:${config.get<string>(
+          'DB_PASSWORD',
+        )}@${config.get<string>('DB_HOST')}:${config.get<string>('DB_PORT')}`,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/config/database.ts
@@ -0,0 +1,8 @@
+export default () => ({
+    database: {
+        host: parseInt(process.env.DB_HOST, 10),
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    }
+})
