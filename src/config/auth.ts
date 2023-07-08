47345974ea77d02a096ee1a26b457abb100ab94f
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-07-08 01:25:10 -0500

    chore: add auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,8 @@
+export default () => ({
+  jwt: {
+    accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+    accessTokenExp: process.env.JWT_ACCESS_TOKEN_EXP,
+    refreshTokenExp: process.env.JWT_REFRESH_TOKEN_EXP,
+  },
+});
