ec8b644efc8ef5dde1f7581264504e470241266f
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-07-08 01:33:18 -0500

    feat: add user services

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+import { CreateUserInput, UpdateUserInput, GetUserInput } from './dto/user.input';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+
+  async createNewUser(createUserInput: CreateUserInput): Promise<HydratedDocument<User>> {
+    const user = await new this.userModel(createUserInput).save();
+    return user;
+  }
+
+  async getUserByEmail(getUserInput: GetUserInput): Promise<HydratedDocument<User>> {
+    const user = await this.userModel.findOne({ email: getUserInput.email }).exec();
+
+    if (!user) {
+      throw new NotFoundException('User not found', {});
+    }
+
+    return user;
+  }
+
+  async getUser(id: string): Promise<HydratedDocument<User>> {
+    return this.userModel.findById(id);
+  }
+}
