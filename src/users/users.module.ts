2d1c25c0b623340217c1f19e344b71f913cb2e6a
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-07-08 01:33:39 -0500

    feat: export user services

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,5 +7,6 @@ import { UsersResolver } from './users.resolver';
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   providers: [UsersResolver, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
