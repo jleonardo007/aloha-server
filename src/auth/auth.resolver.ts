1bdf5371f677e4fe653b526cd1536fdd603ae7ec
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-14 00:47:26 -0500

    feat: add google auth resolvers and services

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -2,6 +2,7 @@ import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { CreateUserInput, GetUserInput } from 'src/users/dto/user.input';
 import { UserOutput } from 'src/users/dto/user.output';
+import { TokenInput } from './dto/auth.input';
 import { LocalAuthGuard } from './guards/local.guard';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { AuthService } from './auth.service';
@@ -19,6 +20,16 @@ export class AuthResolver {
   @UseGuards(LocalAuthGuard)
   async signInWithEmail(@Args('signInInput') signInInput: GetUserInput) {
     return await this.authService.signInWithEmail(signInInput);
+  }
+
+  @Mutation(() => UserOutput)
+  async signUpWithGoogle(@Args('tokenInput') tokenInput: TokenInput) {
+    return await this.authService.signUpWithGoogle(tokenInput);
+  }
+
+  @Query(() => UserOutput)
+  async signInWithGoogle(@Args('tokenInput') tokenInput: TokenInput): Promise<UserOutput> {
+    return await this.authService.signInWithGoogle(tokenInput);
   }
 
   @Query(() => String)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import * as bcrypt from 'bcrypt';
+import { OAuth2Client } from 'google-auth-library';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService, JwtSignOptions } from '@nestjs/jwt';
@@ -6,6 +7,7 @@ import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { CreateUserInput, GetUserInput } from 'src/users/dto/user.input';
 import { UserOutput } from 'src/users/dto/user.output';
+import { TokenInput } from './dto/auth.input';
 
 const enum TokenType {
   access = 'access',
@@ -19,6 +21,8 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
+
+  googleClient = new OAuth2Client(this.configService.get<string>('google.clientId'));
 
   private async encryptPassoword(password: string) {
     const SALT_ROUNDS = 10;
@@ -71,6 +75,30 @@ export class AuthService {
     return await this.createJwtToken(TokenType.access, user);
   }
 
+  async validateUserCredentials(getUserInput: GetUserInput): Promise<User> {
+    const user = await this.userService.getUserByEmail(getUserInput);
+    const validPassword = await this.comparePassword(getUserInput.password, user.password);
+
+    if (validPassword) {
+      return user;
+    } else {
+      throw new UnauthorizedException('Wrong Password');
+    }
+  }
+
+  async getPayloadFormToken(token: TokenInput) {
+    try {
+      const verifiedToken = await this.googleClient.verifyIdToken({
+        idToken: token.tokenFromGoogle,
+        audience: this.configService.get<string>('google.clientId'),
+      });
+
+      return await verifiedToken.getPayload();
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+
   async signUpWithEmail(signUpInput: CreateUserInput): Promise<UserOutput> {
     const newUser = await this.userService.createNewUser({
       email: signUpInput.email,
@@ -87,19 +115,36 @@ export class AuthService {
     };
   }
 
-  async validateUserCredentials(getUserInput: GetUserInput): Promise<User> {
-    const user = await this.userService.getUserByEmail(getUserInput);
-    const validPassword = await this.comparePassword(getUserInput.password, user.password);
-
-    if (validPassword) {
-      return user;
-    } else {
-      throw new UnauthorizedException('Wrong Password');
-    }
-  }
-
   async signInWithEmail(signInInput: GetUserInput): Promise<UserOutput> {
     const user = await this.userService.getUserByEmail(signInInput);
+    const accessToken = await this.createJwtToken(TokenType.access, user);
+
+    return {
+      ...user.toObject(),
+      accessToken,
+    };
+  }
+
+  async signUpWithGoogle(token: TokenInput) {
+    const payload = await this.getPayloadFormToken(token);
+    const newUser = await this.userService.createNewUser({
+      fullName: payload.name,
+      email: payload.email,
+      profilePicture: payload.picture,
+    });
+
+    newUser.refreshToken = await this.createJwtToken(TokenType.refresh, newUser);
+    await newUser.save();
+
+    return {
+      ...newUser.toObject(),
+      accessToken: await this.createJwtToken(TokenType.access, newUser),
+    };
+  }
+
+  async signInWithGoogle(token: TokenInput) {
+    const payload = await this.getPayloadFormToken(token);
+    const user = await this.userService.getUserByEmail({ email: payload.email, password: '' });
     const accessToken = await this.createJwtToken(TokenType.access, user);
 
     return {
