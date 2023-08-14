54b30e38d42df2553483c37500045302e790ae87
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-14 01:17:25 -0500

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
