aa4fd1d609d78387f24c985c374d5ffe8090588b
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-29 23:17:28 -0500

    chore(graphql): change userOutput based on new user schema

--- src/users/dto/user.output.ts
+++ src/users/dto/user.output.ts
@@ -1,8 +1,22 @@
 import { ObjectType, Field, OmitType } from '@nestjs/graphql';
 import { User } from '../entities/user.entity';
-
+import { Chat } from 'src/chats/entities/chat.entity';
+import { Call } from 'src/calls/entities/call.entity';
 @ObjectType()
-export class UserOutput extends OmitType(User, ['password', 'refreshToken']) {
+export class UserOutput extends OmitType(User, [
+  'password',
+  'refreshToken',
+  'sentChats',
+  'receivedChats',
+  'sentCalls',
+  'receivedCalls',
+]) {
   @Field(() => String)
   accessToken: string;
+
+  @Field(() => [Chat])
+  chats: Chat[];
+
+  @Field(() => [Call])
+  calls: Call[];
 }
