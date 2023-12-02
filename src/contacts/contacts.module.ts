d62ed1df7ef463e2fc77ee303028deb3c64fde2e
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-12-01 21:24:28 -0500

    chore: add User and Contact models to contact module

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Contact, ContactSchema } from './entities/contact.entity';
+import { User, UserSchema } from 'src/users/entities/user.entity';
 import { ContactsService } from './contacts.service';
 import { ContactsResolver } from './contacts.resolver';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Contact.name, schema: ContactSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   providers: [ContactsResolver, ContactsService],
 })
 export class ContactsModule {}
